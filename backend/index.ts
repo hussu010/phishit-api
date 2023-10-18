d85f9d81e340d3bd8222c8f82bc704aa2cc9b14a
hussu <hussu010@gmail.com>
2023-10-18 17:35:11 +0545

    seperate out index and server files

--- backend/index.ts
+++ backend/index.ts
@@ -1,15 +1,8 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 5000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("API Server Running on Docker");
-});
-
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-});
+export default app;
