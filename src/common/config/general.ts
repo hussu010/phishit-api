e0641d9718a548edbcf5c32c9b90d476f4e9ff40
hussu <hussu010@gmail.com>
2023-11-26 08:54:55 +0545

    add redirect urls

--- src/common/config/general.ts
+++ src/common/config/general.ts
@@ -3,7 +3,11 @@ const OTP_EXPIRATION_DURATION = 5 * 60 * 1000; // 5 minutes
 const ACCESS_TOKEN_VALIDITY = "1d"; // 1 day
 const REFRESH_TOKEN_VALIDITY = "30d"; // 30 days
 
-const ALLOWED_REDIRECT_URLS = ["http://localhost:3000/auth/google/callback"];
+const ALLOWED_REDIRECT_URLS = [
+  "http://127.0.0.1:3000/auth/google/callback",
+  "https://phishit-ui-dev.tnbswap.com/auth/google/callback",
+  "https://phishit-ui.tnbswap.com/auth/google/callback",
+];
 
 export {
   OTP_EXPIRATION_DURATION,
