3cb64a17121d848b6f3c1d787bfde2e6a64213af
hussu <hussu010@gmail.com>
2023-12-23 12:39:42 +0545

    upgrade nodejs installation

--- scripts/configure_resources.sh
+++ scripts/configure_resources.sh
@@ -1,5 +1,14 @@
 sudo apt update && sudo apt upgrade -y
-curl -s https://deb.nodesource.com/setup_18.x | sudo bash
+
+# install nodejs 18
+sudo apt-get install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=18
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt-get update
+sudo apt-get install nodejs -y
+
 sudo apt install nodejs nginx ufw software-properties-common certbot python3-certbot-nginx -y
 sudo ufw enable
 sudo ufw status
