f96f7391db11c823cf8326988b0671298b78b268
hussu <hussu010@gmail.com>
2023-12-28 22:25:40 +0545

    Add localhost redirect URL for Google OAuth and payment

--- src/common/config/general.ts
+++ src/common/config/general.ts
@@ -5,11 +5,13 @@ const REFRESH_TOKEN_VALIDITY = "30d"; // 30 days
 
 const ALLOWED_GOOGLE_OAUTH_REDIRECT_URLS = [
   "http://127.0.0.1:3000/auth/google/callback",
+  "http://localhost:3000/auth/google/callback",
   "https://phishit-ui-dev.tnbswap.com/auth/google/callback",
   "https://phishit-ui.tnbswap.com/auth/google/callback",
 ];
 
 const ALLOWED_PAYMENT_REDIRECT_URLS = [
+  "http://127.0.0.1:3000/bookings/payment",
   "http://localhost:3000/bookings/payment",
   "https://phishit-ui-dev.tnbswap.com/bookings/payment",
   "https://phishit-ui.tnbswap.com/bookings/payment",
